2f44be21c067bd54155f425eacb07c1879422208
keygx <y.kagiyama@gmail.com>
2015-08-02 03:55:48 +0900

    Modified to XCTest Extension

--- README.md
+++ README.md
@@ -2,69 +2,9 @@
 
 Power Assert inspired debug tool in Swift
 
-* ViewController.swift -> viewDidLoad()
-
-```ViewController.sfift
-20: let num1 = 0
-21: let num2 = 10
-22: PAssert.assert(num1, >, num2) // false
-23: PAssert.assert(num2, ==, 10)  // true
-```
-
-* Xcode Debug Area
-
-```
-=== Assertion Failed ===========================================================
-DATE: 2015-04-09 17:02:04
-FILE: ViewController.swift
-LINE: 22
-FUNC: viewDidLoad()
-
-=> PAssert.assert(num1, >, num2)
-                  |     |  |
-                  |     |  10
-                  |     |
-                  |     false
-                  |
-                  0
-
-[2015-04-09 17:02:04 ViewController.swift:23 viewDidLoad()] 10
-```
-
 ## Requirements
 - Swift 1.2
 - iOS 8.0 or later
-
-## Installation
-
-###CocoaPods
-
-* PodFile
-
-```POdFile
-source 'https://github.com/CocoaPods/Specs.git'
-platform :ios, '8.0'
-use_frameworks!
-
-pod 'PAssert', :git => 'https://github.com/keygx/PAssert.git'
-```
-* install
-
-```
-$ pod install
-```
-
-
-## Usage
-
-###Add DEBUG flag.
-
-- installed by CocoaPods
-
-Pods > TARGETS > Pods-PAssert > Swift Compiler - Custom Flags > Other Swift Flags > Debug > -DDEBUG
-
-![xcode](https://qiita-image-store.s3.amazonaws.com/0/15905/0d527e94-c83d-817d-e5ea-9b45ba542ea0.png)
-<http://qiita.com/keygx/items/8c88cc1a39bb452c883f>
 
 ## License
 
--- Source/XCTest+PAssert.swift
+++ Source/XCTest+PAssert.swift
@@ -1,119 +1,101 @@
 //
-//  PAssert.swift
-//  PAssert
+//  XCTest+PAssert.swift
 //
-//  Created by keygx on 2015/03/20.
+//  Created by keygx on 2015/08/01.
 //  Copyright (c) 2015年 keygx. All rights reserved.
 //
 
 import Foundation
+import XCTest
 
-public class PAssert {
+extension XCTest {
 
-    /**
-        Setting at Xcode :
-        ---------------------------------------
-        > Other Swift Flags > Debug  -D DEBUG
-    
-
-        Usage :
-        ---------------------------------------
-        #if DEBUG
-            // Only Debugging
-        #endif
-    */
-
-
-    // MARK: - assert
-    public class func assert<T>(lhs: T, _ comparison: (T, T) -> Bool, _ rhs: T,
-                filePath: String = __FILE__, lineNumber: Int = __LINE__, function: String = __FUNCTION__) {
-        #if DEBUG
-
-            let result = comparison(lhs, rhs)
-
-            if !result {
-                var source = readSource(filePath)
-
-                if source != "" {
-                    source = removeComment(source)
-                    source = removeMultilinesComment(source)
-                    output(source: source, comparison: result, lhs: lhs, rhs: rhs, fileName: getFilename(filePath), lineNumber: lineNumber, function: function)
-
-                } else {
-                    return
-                }
-
-            } else {
-                println("[\(getDateTime()) \(getFilename(filePath)):\(lineNumber) \(function)] \(lhs)")
+    // MARK: - PAssert
+    func PAssert<T>(@autoclosure lhs: () -> T, _ comparison: (T, T) -> Bool, @autoclosure _ rhs: () -> T, filePath: String = __FILE__, lineNumber: Int = __LINE__, function: String = __FUNCTION__) {
+        
+        let result = comparison(lhs(), rhs())
+        
+        if !result {
+            var source = readSource(filePath)
+            
+            if source != "" {
+                source = removeComment(source)
+                source = removeMultilinesComment(source)
+                let out = output(source: source, comparison: result, lhs: lhs(), rhs: rhs(), fileName: getFilename(filePath), lineNumber: lineNumber, function: function)
+                
+                XCTFail(out, file: filePath, line:UInt(lineNumber))
             }
-
-        #endif
+        } else {
+            println("")
+            println("[\(getDateTime()) \(getFilename(filePath)):\(lineNumber) \(function)] \(lhs())")
+            println("")
+        }
     }
-
+    
     // MARK: - get datetime
-    private class func getDateTime() -> String {
+    private func getDateTime() -> String {
         let now = NSDate()
         let dateFormatter = NSDateFormatter()
         let localeIdentifier = NSLocale.currentLocale().localeIdentifier
         dateFormatter.locale = NSLocale(localeIdentifier: localeIdentifier)
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-
+        
         return dateFormatter.stringFromDate(now)
     }
-
+    
     // MARK: - read source file
-    private class func readSource(filePath: String) -> String {
+    private func readSource(filePath: String) -> String {
         var error : NSError?
         var source = String(contentsOfFile:filePath, encoding: NSUTF8StringEncoding, error: &error)
-
+        
         if error != nil {
             println("error: \(error)")
         }
-
+        
         if let str = source {
             return str
         } else {
             return ""
         }
     }
-
+    
     // MARK: - remove comments
-    private class func removeComment(source: String) -> String {
+    private func removeComment(source: String) -> String {
         var formatted = ""
-
+        
         let pattern = "[ \t]*//.*"
         let replace = ""
         formatted = source.stringByReplacingOccurrencesOfString(pattern, withString: replace, options: NSStringCompareOptions.RegularExpressionSearch, range: nil)
-
+        
         return formatted
     }
-
+    
     // MARK: - remove multiline comments
-    private class func removeMultilinesComment(source: String) -> String {
+    private func removeMultilinesComment(source: String) -> String {
         var formatted = ""
-
+        
         let pattern = "/\\*.*?\\*/"
         let replace = ""
         formatted = source.stringByReplacingOccurrencesOfString(pattern, withString: replace, options: NSStringCompareOptions.RegularExpressionSearch, range: nil)
-
+        
         return formatted
     }
-
+    
     // MARK: - get literal
-    private class func getLiteral(source: String, lineNumber: Int) -> String {
+    private func getLiteral(source: String, lineNumber: Int) -> String {
         var tmpLine = ""
         var literal = ""
         var lineIndex = 1
         var startBracket = 0
         var endBracket = 0
         var comma = 0
-
+        
         source.enumerateLines {
             line, stop in
-
+            
             if lineIndex >= lineNumber {
                 tmpLine = line.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-
+                
                 for char in tmpLine {
                     if char == "(" {
                         ++startBracket
@@ -132,26 +114,26 @@ public class PAssert {
             }
             lineIndex += 1
         }
-
+        
         return literal
     }
-
+    
     // MARK: - formatt literal
-    private class func formattLiteral(literal: String) -> String {
+    private func formattLiteral(literal: String) -> String {
         var formatted = ""
-
+        
         let pattern = "(,\\s*)"
         let replace = ", "
         formatted = literal.stringByReplacingOccurrencesOfString(pattern, withString: replace, options: NSStringCompareOptions.RegularExpressionSearch, range: nil)
-
+        
         return formatted
     }
-
+    
     // MARK: - get comma indexes
-    private class func getIndexes(literal: String) -> Array<Int> {
+    private func getIndexes(literal: String) -> Array<Int> {
         var indexes: Array<Int> = []
         var i = 0
-
+        
         for char in literal {
             if char == "(" {
                 if indexes.count == 0 {
@@ -162,88 +144,86 @@ public class PAssert {
             }
             ++i
         }
-
+        
         return indexes
     }
-
+    
     // MARK: - get file name
-    private class func getFilename(filePath: String) -> String {
+    private func getFilename(filePath: String) -> String {
         var fileName = ""
-
+        
         if let match = filePath.rangeOfString("[^/]*$", options: .RegularExpressionSearch) {
             fileName = filePath.substringWithRange(match)
         }
-
+        
         return fileName
     }
-
+    
     // MARK: - repeat character
-    private class func repeatCharacter(char: String, length: Int) -> String {
+    private func repeatCharacter(char: String, length: Int) -> String {
         var chars = ""
-
+        
         if length > 0 {
             for i in 0..<length {
                 chars += char
             }
         }
-
+        
         return chars
     }
-
+    
     // MARK: - print result
-    private class func output<T>(#source: String?, comparison: Bool, lhs: T, rhs: T, fileName: String, lineNumber: Int, function: String) {
-
+    private func output<T>(#source: String?, comparison: Bool, lhs: T, rhs: T, fileName: String, lineNumber: Int, function: String) -> String {
+        
+        var out = ""
         var literal = ""
         if var src = source {
             literal = getLiteral(src, lineNumber: lineNumber)
         }
         literal = "=> " + formattLiteral(literal)
-
+        
         let indexes = getIndexes(literal)
-
+        
         let file = "FILE: \(fileName)"
         let title = "=== Assertion Failed ==========================================================="
-
-        println("\(title)")
-        println("DATE: \(getDateTime())")
-        println(file)
-        println("LINE: \(lineNumber)")
-        println("FUNC: \(function)")
-
+        
+        out += "\n\n\(title)\n"
+        out += "DATE: \(getDateTime())\n"
+        out += "\(file)\n"
+        out += "LINE: \(lineNumber)\n"
+        out += "FUNC: \(function)\n"
+        
         if indexes.count == 3 {
-            println("")
-            println(literal)
-
+            
+            out += "\n"
+            out += "\(literal)\n"
+            
             var lValue = "\(lhs)"
             lValue = (lValue == "") ? "\"\"" : lValue
-
+            lValue = "\(lValue) [\(reflect(lhs).summary)]"
+            
             var rValue = "\(rhs)"
             rValue = (rValue == "") ? "\"\"" : rValue
-
+            rValue = "\(rValue) [\(reflect(rhs).summary)]"
+            
             var space1 = repeatCharacter(" ", length: indexes[0] + 1)
             var space2 = repeatCharacter(" ", length: indexes[1] - indexes[0])
             var space3 = repeatCharacter(" ", length: indexes[2] - indexes[1] - 1)
-
-            println("\(space1)|\(space2)|\(space3)|")
-
-            println("\(space1)|\(space2)|\(space3)\(rValue)")
-
-            println("\(space1)|\(space2)|")
-
-            println("\(space1)|\(space2)\(comparison)")
-
-            println("\(space1)|")
-
-            println("\(space1)\(lValue)")
-
-            println("")
-
-            //abort()
-
+            
+            out += "\(space1)|\(space2)|\(space3)|\n"
+            out += "\(space1)|\(space2)|\(space3)\(rValue)\n"
+            out += "\(space1)|\(space2)|\n"
+            out += "\(space1)|\(space2)\(comparison)\n"
+            out += "\(space1)|\n"
+            out += "\(space1)\(lValue)\n"
+            
         } else {
-            println("")
-            println("=> pa.assert(... \n///// Could not output /////")
-            println("")
+            out += "\n"
+            out += "=> pa.assert(... \n///// Could not output /////\n"
+            out += "\n"
         }
+        
+        return out
     }
+    
 }
